ac461526a90ba44b9ebb2d8c535b019e15f0c8d8
Exodin1 <exodin1@outlook.com>
2021-08-24 14:38:33 -0300

    mockFunctions

--- src/mockFunctions.js
+++ src/mockFunctions.js
@@ -1,26 +1,19 @@
 // ATENÇÃO!!! Não edite este arquivo. Altere apenas os arquivos da pasta 'test'.
-const multiply = (a, b) => a / b;
+const multiply = (a, b) => a * b;
 
-const power = (a, b) => {
-  let pow = a;
-  for (let i = 0; i < b; i += 1) {
-    pow = multiply(pow, a);
-  }
-  return pow;
-};
+const power = (a, b) => a ** b;
 
 const factorial = (a) => {
-  let fact = 0;
-  for (let i = 0; i < a; i += 1) {
-    fact += a;
-  }
-  return fact;
+  if (a < 0) return -1;
+  if (a === 0) return 1;
+
+  return (a * factorial(a - 1));
 };
 
-const add = (a, b) => a - b;
+const add = (a, b) => a + b;
 
-const subtract = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 
-const divide = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 module.exports = { add, subtract, multiply, divide, power, factorial };
